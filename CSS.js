d1fdeb0747aa8fb2b9ed10a331e004d024a85810
JoshuaNguyenAPIU <128469065+JoshuaNguyenAPIU@users.noreply.github.com>
2023-05-03 15:23:09 +0700

    Add files via upload

---
+++ CSS.js
@@ -0,0 +1,156 @@
+/* Global Styles */
+
+body {
+    font-family: "Poppins", sans-serif;
+    font-size: 16px;
+    line-height: 1.5;
+    color: #333;
+    background-color: #f8f8f8;
+  }
+  
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 600;
+    margin-top: 0;
+  }
+  
+  a {
+    color: #007bff;
+    text-decoration: none;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
+  
+  .btn-primary {
+    background-color: #007bff;
+    border-color: #007bff;
+  }
+  
+  .btn-primary:hover {
+    background-color: #0069d9;
+    border-color: #0062cc;
+  }
+  
+  /* Header Styles */
+  
+  .navbar {
+    background-color: #fff;
+    border-bottom: 1px solid rgba(0,0,0,.1);
+    box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
+  }
+  
+  .nav-link {
+    font-size: 1.2rem;
+    font-weight: 500;
+    padding: .5rem 1rem;
+  }
+  
+  .nav-link:hover {
+    background-color: #f8f8f8;
+  }
+  
+  /* Hero Section Styles */
+  
+  .hero {
+    height: 100vh;
+    min-height: 500px;
+    background-image: url("https://picsum.photos/1920/1080");
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+  }
+  
+  .hero h1 {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
+  
+  .hero p {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+  
+  .hero button {
+    font-size: 1.2rem;
+  }
+  
+  /* Services Section Styles */
+  
+  .services {
+    padding: 4rem 0;
+    background-color: #fff;
+  }
+  
+  .services h2 {
+    font-size: 2.5rem;
+    text-align: center;
+    margin-bottom: 4rem;
+  }
+  
+  .services .card {
+    border: none;
+  }
+  
+  .services .card img {
+    height: 150px;
+  }
+  
+  .services .card h3 {
+    font-size: 1.8rem;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+  
+  .services .card p {
+    font-size: 1.2rem;
+    line-height: 1.6;
+    margin-bottom: 2rem;
+  }
+  
+  /* About Section Styles */
+  
+  .about {
+    padding: 4rem 0;
+    background-color: #f8f8f8;
+  }
+  
+  .about h2 {
+    font-size: 2.5rem;
+    text-align: center;
+    margin-bottom: 4rem;
+  }
+  
+  .about img {
+    height: 250px;
+    width: 250px;
+    margin-bottom: 2rem;
+  }
+  
+  .about p {
+    font-size: 1.2rem;
+    line-height: 1.6;
+    margin-bottom: 2rem;
+  }
+  
+  /* Contact Section Styles */
+  
+  .contact {
+    padding: 4rem 0;
+    background-color: #fff;
+  }
+  
+  .contact h2 {
+    font-size: 2.5rem;
+    text-align: center;
+    margin-bottom: 4rem;
+  }
+  
+  .contact form label {
+    font-size: 1.2rem;
+    font-weight: 500
+  
---
+++ script.js
@@ -0,0 +1,10 @@
+// Get the login button element
+const loginButton = document.getElementById('login-button');
+
+// Add a click event listener to the button
+loginButton.addEventListener('click', () => {
+  // Redirect to the login.html subpage
+  window.location.href = 'login.html';
+});
+
+
